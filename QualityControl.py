3e3b95d910d4d2445925808bdc411bcd231e2d1e
AJSomper <A.J.Somper-21@student.lboro.ac.uk>
2025-02-06 12:24:24 +0000

    Created QualityControl.py
    Created a new file for the quality control screen.

---
+++ QualityControl.py
@@ -0,0 +1,12 @@
+#Reset All
+    #Reset all fields on the form to their default values
+
+#DMeans --> Calculate Daily Stats
+
+#OutliersID --> Calulcate outliers
+
+#QualityCheck --> Check File
+
+#PettittTest --> Called when check file is run
+
+#IncreaseDate --> Called in PettittTest

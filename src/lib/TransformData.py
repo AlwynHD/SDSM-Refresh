b3c08ebbbc1ba5a87c9d9b889267ed75dd3cf6a0
AJSomper <A.J.Somper-21@student.lboro.ac.uk>
2025-03-13 16:19:39 +0000

    Added boxcox and unboxcox to transform data
    Added each function using the scipy package. Still need to check with Chris to confirm some details.

--- src/lib/TransformData.py
+++ src/lib/TransformData.py
@@ -1,7 +1,15 @@
 import numpy as np
 import datetime as dt
 import scipy as sci
-from src.lib.utils import loadFilesIntoMemory, selectFile
+try:
+    from src.lib.utils import loadFilesIntoMemory, selectFile
+except ModuleNotFoundError:
+    from utils import loadFilesIntoMemory, selectFile
+
+globalSDate = dt.date(1948, 1, 1)
+globalEDate = dt.date(2015, 12, 31)
+globalMissingCode = -999
+thresh = 0
 
 def loadData(file):
     data = loadFilesIntoMemory(file)[0]
@@ -12,14 +20,14 @@ def loadData(file):
     data.astype(np.longdouble)
     return data
 
-def valueIsValid(value):
+def valueIsValid(value, applyThresh):
     return value != globalMissingCode and (not applyThresh or value > thresh)
 
 def genericTransform(data, func):
     returnData = np.empty_like(data)
     for c in range(len(data.T)):
         for r in range(len(data[:, c])):
-            if valueIsValid(data[r][c]):
+            if valueIsValid(data[r][c], applyThresh):
                 returnData[r][c] = func(data[r][c])
             else:
                 returnData[r][c] = data[r][c]
@@ -41,8 +49,8 @@ def backwardsChange(data):
     returnData = np.empty_like(data)
     for c in range(len(data.T)):
         for r in range(len(data[:, c])):
-            if valueIsValid(data[r][c]):
-                if r == 0 or not valueIsValid(data[r - 1][c]):
+            if valueIsValid(data[r][c], applyThresh):
+                if r == 0 or not valueIsValid(data[r - 1][c], applyThresh):
                     returnData[r][c] = globalMissingCode
                 else:
                     returnData[r][c] = data[r][c] - data[r - 1][c]
@@ -62,7 +70,7 @@ def binomial(data, binomial):
     returnData = np.empty_like(data)
     for c in range(len(data.T)):
         for r in range(len(data[:, c])):
-            if valueIsValid(data[r][c]):
+            if valueIsValid(data[r][c], applyThresh):
                 returnData[r][c] = 1 if data[r][c] > binomial else 0
             else:
                 returnData[r][c] = data[r][c]
@@ -71,7 +79,7 @@ def binomial(data, binomial):
 def extractEnsemble(data, column):
     return data[:, column - 1]
 
-def padData(data):
+def padData(data, dataSDate, dataEDate):
     startDiff = dataSDate - globalSDate
     endDiff = globalEDate - dataEDate
     return np.pad(data, [(startDiff.days, endDiff.days), (0, 0)], mode='constant', constant_values=globalMissingCode)
@@ -80,36 +88,74 @@ def removeOutliers(data, sdFilterValue):
     returnData = np.empty_like(data)
     for c in range(len(data.T)):
         column = data[:, c]
-        threshCol = [entry for entry in column if valueIsValid(entry)]
+        threshCol = [entry for entry in column if valueIsValid(entry, applyThresh)]
         if len(threshCol) > 0:
             mean = sum(threshCol) / len(threshCol)
             sd = 0
             for entry in threshCol:
-                if valueIsValid(entry):
+                if valueIsValid(entry, applyThresh):
                     sd += np.power((entry - mean), 2)
             sd = np.sqrt(sd / len(threshCol))
             sdFilter = sd * sdFilterValue
         
         filteredCol = np.empty_like(column)
         for r in range(len(column)):
-            if valueIsValid(column[r]) and (column[r] > (mean + sdFilter) or column[r] < (mean - sdFilter)):
+            if valueIsValid(column[r], applyThresh) and (column[r] > (mean + sdFilter) or column[r] < (mean - sdFilter)):
                 filteredCol[r] = globalMissingCode
             else:
                 filteredCol[r] = column[r]
         returnData[:, c] = filteredCol
     return returnData
 
+def boxCox(data):
+    #todo check with Chris if we can use auto generated lambda or if he wants his method
+    #todo also check if he will let us boxcox data with fewer than 50 entries
+    #todo and if we can run this for multiple columns
+    returnData = np.empty_like(data)
+    for c in range(len(data.T)):
+        boxCoxData = [entry for entry in data[:, c] if valueIsValid(entry, applyThresh)]
+        boxCoxData = sci.stats.boxcox(boxCoxData)[0]
+
+        invalidCount = 0
+        for r in range(len(data[:, c])):
+            if valueIsValid(data[r][c], applyThresh):
+                returnData[r][c] = boxCoxData[r - invalidCount]
+            else:
+                returnData[r][c] = data[r][c]
+                invalidCount += 1
+    return returnData
+
+def unBoxCox(data, lamda, leftShift):
+    returnData = np.empty_like(data)
+    for c in range(len(data.T)):
+        unBoxCoxData = [entry for entry in data[:, c] if valueIsValid(entry, applyThresh)]
+        for i in range(len(unBoxCoxData)):
+            unBoxCoxData[i] += leftShift
+
+        unBoxCoxData = sci.special.inv_boxcox(unBoxCoxData, lamda)
+        for i in range(len(unBoxCoxData)):
+            unBoxCoxData[i] -= leftShift
+
+        invalidCount = 0
+        for r in range(len(data[:, c])):
+            if valueIsValid(data[r][c], applyThresh):
+                returnData[r][c] = unBoxCoxData[r - invalidCount]
+            else:
+                returnData[r][c] = data[r][c]
+                invalidCount += 1
+    return returnData
+
 if __name__ == "__main__":
-    """Variables that are gotten from the settings or the screen."""
-    file = selectFile()
-    globalSDate = dt.date(1948, 1, 1)
-    globalEDate = dt.date(2015, 12, 31)
+    """Variables that are gotten from the screen."""
+    applyThresh = True
     dataSDate = dt.date(1948, 1, 1)
     dataEDate = dt.date(2015, 12, 31)
-    globalMissingCode = -999
-    thresh = 0
-    applyThresh = True
+    lagValue = 1
+    binomialValue = 1
+    ensembleCol = 1
+    sdFilter = 1
 
+    file = selectFile()
     data = loadData(file)
 
     genericTransform(data, np.log)
@@ -127,14 +173,14 @@ if __name__ == "__main__":
     genericTransform(data, returnSelf)
 
     backwardsChange(data)
-    lag(data, 1)
-    binomial(data, 1)
+    lag(data, lagValue)
+    binomial(data, binomialValue)
 
-    extractEnsemble(data, 1)
+    extractEnsemble(data, ensembleCol)
 
-    padData(data)
+    padData(data, dataSDate, dataEDate)
 
-    removeOutliers(data)
+    removeOutliers(data, sdFilter)
 
-    sci.stats.boxcox(data)
-    sci.special.inv_boxcox(data, 1)
+    boxCox(data)
+    print(unBoxCox(data, 2, 5))

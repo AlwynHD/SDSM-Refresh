2b2a127b16704d165f7793cd481081cef748e209
AJSomper <A.J.Somper-21@student.lboro.ac.uk>
2025-03-27 15:13:25 +0000

    Quality Control now gets settings.
    Quality control now reads the settings file for global missing code, threshold, and global start date.

--- src/lib/QualityControl.py
+++ src/lib/QualityControl.py
@@ -3,29 +3,14 @@ import datetime
 import math
 import numpy as np
 try:
-    from src.lib.utils import loadFilesIntoMemory, increaseDate, checkForFile, checkIfFileFormatted, selectFile
+    from src.lib.utils import loadFilesIntoMemory, selectFile, getSettings
 except ModuleNotFoundError:
-    from utils import loadFilesIntoMemory, increaseDate, checkForFile, checkIfFileFormatted, selectFile
+    from utils import loadFilesIntoMemory, selectFile, getSettings
 
-#Local version
-predictorSelected = ['predictor files/ncep_dswr.dat']
-predictandSelected = ['predictand files/NoviSadPrecOBS.dat']
+def valueIsValid(value, applyThresh, missingCode, thresh):
+    return value != missingCode and (not applyThresh or value > thresh)
 
-#Currently accessing local file, change as needed for your own version.
-selectedFile = "predictand files/NoviSadPrecOBS.dat"
-outlierFile = "outlier.txt"
-
-#Variables obtained from global settings
-globalSDate = datetime.date(1948, 1, 1)
-leapYear = True
-globalMissingCode = -999
-applyThresh = False
-thresh = 0
-
-def valueIsValid(value, applyThresh):
-    return value != globalMissingCode and (not applyThresh or value > thresh)
-
-def dailyMeansNew(filePath, applyThresh):
+def dailyMeans(filePath, applyThresh, missingCode, thresh, globalSDate):
     data = loadFilesIntoMemory(filePath)[0]
     dailyStats = np.zeros((7, 4), float)
     #[i][0]: sum, [i][1]: count, [i][2]: mean, [i][3] standard deviation
@@ -34,23 +19,23 @@ def dailyMeansNew(filePath, applyThresh):
     #Mean
     day = globalSDate.day
     for value in data:
-        if valueIsValid(value, applyThresh):
+        if valueIsValid(value, applyThresh, missingCode, thresh):
             dailyStats[day][0] += value
             dailyStats[day][1] += 1
             day = (day + 1) % 7
 
     for stat in dailyStats:
-        stat[2] = stat[0] / stat[1] if stat[1] > 0 else globalMissingCode
+        stat[2] = stat[0] / stat[1] if stat[1] > 0 else missingCode
 
     #Standard Deviation
     day = globalSDate.day
     for value in data:
-        if dailyStats[day][2] != globalMissingCode and valueIsValid(value, applyThresh):
+        if dailyStats[day][2] != missingCode and valueIsValid(value, applyThresh, missingCode, thresh):
             dailyStats[day][3] += (value - dailyStats[day][2]) ** 2
             day = (day + 1) % 7
 
     for stat in dailyStats:
-        stat[3] = math.sqrt(stat[3] / stat[1]) if stat[1] > 0 else globalMissingCode
+        stat[3] = math.sqrt(stat[3] / stat[1]) if stat[1] > 0 else missingCode
 
     #Output
     output = ""
@@ -59,14 +44,14 @@ def dailyMeansNew(filePath, applyThresh):
 
     return output
 
-def getOutliersNew(filePath, outlierFile, sdFilterValue, applyThresh):
+def getOutliers(filePath, outlierFile, sdFilterValue, applyThresh, missingCode, thresh):
     data = loadFilesIntoMemory(filePath)[0]
-    workingData = [value for value in data if valueIsValid(value, applyThresh)]
+    workingData = [value for value in data if valueIsValid(value, applyThresh, missingCode, thresh)]
     if len(workingData) > 0:
             mean = sum(workingData) / len(workingData)
             sd = 0
             for value in workingData:
-                if valueIsValid(value, applyThresh):
+                if valueIsValid(value, applyThresh, missingCode, thresh):
                     sd += np.power((value - mean), 2)
             sd = np.sqrt(sd / len(workingData))
             sdFilter = sd * sdFilterValue
@@ -74,7 +59,7 @@ def getOutliersNew(filePath, outlierFile, sdFilterValue, applyThresh):
     index = []
     outliers = []
     for i in range(len(data)):
-        if valueIsValid(data[i], applyThresh) and (data[i] > (mean + sdFilter) or data[i] < (mean - sdFilter)):
+        if valueIsValid(data[i], applyThresh, missingCode, thresh) and (data[i] > (mean + sdFilter) or data[i] < (mean - sdFilter)):
             index.append(i + 1)
             outliers.append(data[i])
 
@@ -86,23 +71,23 @@ def getOutliersNew(filePath, outlierFile, sdFilterValue, applyThresh):
 
     return str(len(outliers)) + " outliers identified and written to file."
 
-def qualityCheckNew(filePath, applyThresh):
+def qualityCheck(filePath, applyThresh, missingCode, thresh):
     data = loadFilesIntoMemory(filePath)[0]
     totalCount = len(data)
-    missingCount = sum(1 if entry == globalMissingCode else 0 for entry in data)
+    missingCount = sum(1 if entry == missingCode else 0 for entry in data)
     okCount = totalCount - missingCount
-    threshCount = sum(1 if valueIsValid(entry, True) else 0 for entry in data)
+    threshCount = sum(1 if valueIsValid(entry, True, missingCode, thresh) else 0 for entry in data)
     
-    validData = [entry for entry in data if valueIsValid(entry, applyThresh)]
+    validData = [entry for entry in data if valueIsValid(entry, applyThresh, missingCode, thresh)]
     dataSum = sum(validData)
     dataMax = max(validData)
     dataMin = min(validData)
 
-    prevValue = globalMissingCode
+    prevValue = missingCode
     maxDifference = -9999
     for i in range(len(data)):
-        if prevValue != globalMissingCode:
-            if (valueIsValid(data[i], applyThresh)):
+        if prevValue != missingCode:
+            if (valueIsValid(data[i], applyThresh, missingCode, thresh)):
                 difference = abs(prevValue - data[i])
                 if difference > maxDifference:
                     maxDifference = round(difference, 4) #Floating point calc
@@ -117,9 +102,9 @@ def qualityCheckNew(filePath, applyThresh):
     elif okCount > 0:
         mean = round(dataSum / okCount, 4)
     else:
-        mean = globalMissingCode
+        mean = missingCode
 
-    return dataMin, dataMax, mean, totalCount, missingCount, okCount, maxDifference, maxDiffVal1, maxDiffVal2, threshCount, 0, 0, globalMissingCode, thresh
+    return dataMin, dataMax, mean, totalCount, missingCount, okCount, maxDifference, maxDiffVal1, maxDiffVal2, threshCount, 0, 0, missingCode, thresh
 
 def pettittTest(data, ptPercent):
     place = "holder"
@@ -151,7 +136,15 @@ if __name__ == '__main__':
     applyThresh = False
     standardDeviations = 5
 
-    print(getOutliersNew(filePath, r"C:\Users\ajs25\Downloads\outliers.dat", standardDeviations, applyThresh))
+    settings = getSettings()
+
+    missingCode = settings["globalmissingcode"]
+    thresh = settings["fixedthreshold"]
+    globalSDate = settings["globalsdate"]
+
+    #print(dailyMeans(filePath, applyThresh, missingCode, thresh, globalSDate))
+    #print(getOutliers(filePath, r"C:\Users\ajs25\Downloads\outliers.dat", standardDeviations, applyThresh, missingCode, thresh))
+    #print(qualityCheck(filePath, applyThresh, missingCode, thresh))
 
     data = [2, 3, 4, 5, 6, 7, 8, 9, 10, 100, 120, 145, 145, 555, 444, 333, 333, 333, 333, 333]
     #print(performPettittTest(data))
